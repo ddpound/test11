53115276b70dd4feeb6eb2082caf61fb80292836
dpound <dpound@naver.com>
2021-02-16 14:27:37 +0900

    기초 테스트 뼈대 프로젝트 입니다.

---
+++ src/test11/testCar.java
@@ -0,0 +1,8 @@
+package test11;
+
+public class testCar {
+	public testCar() {
+		System.out.println("test car 생성자입니다");
+	}
+
+}
---
+++ src/test11/testMain.java
@@ -0,0 +1,10 @@
+package test11;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		System.out.println("main 입니다");
+
+	}
+
+}
