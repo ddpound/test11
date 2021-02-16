3251f6fa4339ffd71c9911fcbe4825dc7f47877c
dpound <dpound@naver.com>
2021-02-16 14:30:19 +0900

    인터페이스 작성해서 올렸습니다.

---
+++ src/test11/util.java
@@ -0,0 +1,11 @@
+package test11;
+
+// 기본 기능들의 뼈대를 위한 인터페이스입니다 
+// 메소드 생성하려면 꼭 여기에 등록해주세요
+public interface util {
+	
+	//-- 임시 양식 입니다 
+	public void Car();
+	public String H(int test); // 스트링 출력
+	public String Dog(int input); // 강아지의 짖는걸 출력
+}
